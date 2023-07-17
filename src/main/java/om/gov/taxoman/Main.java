7f2e0ec0a8265b360c8f1f69891bbe0eb3c6eef0
Manarz97 <138357870+manarz97@users.noreply.github.com>
2023-07-17 14:17:18 +0400

    commit

--- src/main/java/om/gov/taxoman/Main.java
+++ src/main/java/om/gov/taxoman/Main.java
@@ -4,19 +4,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import om.gov.taxoman.Entity.Property;
+import om.gov.taxoman.Entity.*;
 
+import java.lang.module.FindException;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        Property property =new Property();
-        property.setName("Jeep");
-        property.setDayRentalPrice(99.50);
-        property.setId(11);
-
+//        Property property =new Property();
+//        property.setName("Jeep");
+//        property.setDayRentalPrice(99.50);
+//        Property property1 =new Property();
+//        property1.setName("Jeep");
+//        property1.setDayRentalPrice(80.50);
+//
+//
+//
+//        Customer customer=new Customer();
+//        customer.setFullName("manar qasim alqasmi");
+//        customer.setEmail("manar@gmail.com");
+//        customer.setPassword("manar");
+//
+//        Customer customer1=new Customer();
+//        customer1.setFullName("rayyan qasim alqasmi");
+//        customer1.setEmail("rayyan@gmail.com");
+//
+//
+//        PropertyOwner propertyOwner=new PropertyOwner();
+//        propertyOwner.setEmail("manar@gmail.com");
+//        propertyOwner.setPassword("24252");
+//        propertyOwner.setPhoneNumber("98398938");
+//        PropertyOwner propertyOwner1=new PropertyOwner();
+//        propertyOwner1.setEmail("rayyan@gmail.com");
+//        propertyOwner1.setPassword("242mmm52");
+//        propertyOwner1.setPhoneNumber("9999999");
 
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("manar");
@@ -24,9 +49,25 @@ public class Main {
         em.getTransaction() .begin();
 
 
-        em.persist(property);
+//        em.persist(property);
+//        em.persist(property1);
+//        em.persist(customer);
+//        em.persist(customer1);
+//        em.persist(propertyOwner);
+//        em.persist(propertyOwner1);
+        Customer customer = em.find(Customer.class,1);
+        Property property = em.find(Property.class,1);
+
+        Rental rental = new Rental();
+        rental.setCustomer(customer);
+        rental.setProperty(property);
+        rental.setStartDate(LocalDate.now());
+        rental.setEndDate(LocalDate.now());
+        rental.setRentingAmount(52);
 
 
+
+       em.persist(rental);
 
 
         em.getTransaction() .commit();
